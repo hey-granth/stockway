506e94d98b5bdd6f0fce907dbed89118e1fe56b7
Granth Agarwal <heygranth@gmail.com>
2025-10-15 21:41:54 +0530

    migrated from sqlite to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": Config.DB_NAME,
+        "USER": Config.DB_USER,
+        "PASSWORD": Config.DB_PASSWORD,
+        "HOST": Config.DB_HOST,
+        "PORT": Config.DB_PORT,
     }
 }
 
--- configs/config.py
+++ configs/config.py
@@ -7,3 +7,11 @@ load_dotenv()
 class Config:
     SECRET_KEY = getenv("SECRET_KEY")
     DEBUG = getenv("DEBUG", "False")
+
+    DATABASE_URL = getenv("DATABASE_URL")
+    # Add other configuration variables as needed
+    DB_USER = getenv("DB_USER")
+    DB_PASSWORD = getenv("DB_PASSWORD")
+    DB_HOST = getenv("DB_HOST", "localhost")
+    DB_PORT = getenv("DB_PORT", "5432")
+    DB_NAME = getenv("DB_NAME")
